71da4ee5731bb41b21e3f5340c3ad39fdfae031f
maye12emata <155933437+maye12emata@users.noreply.github.com>
2024-01-08 11:42:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner')
-streamlit.Header('Breakfast Menu')
-streamlit.Text('Omega 3 & Blueberry Oatmeal')
-streamlit.Text('Kale, Spinach & Rocket Smoothie')
-streamlit.Text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
