162fa2df8452551875e189d9f02131487d4b61a6
maye12emata <155933437+maye12emata@users.noreply.github.com>
2024-01-09 23:48:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ try:
    streamlit.dataframe(back_from_function)
     
 
-
+#dont run anything past here while we troubleshoot
+streamlit.stop()  
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
