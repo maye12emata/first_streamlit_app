4a22f6dd894cf91097b59cb4ec83dd9b9ccb6c3a
maye12emata <155933437+maye12emata@users.noreply.github.com>
2024-01-09 23:34:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,10 +56,11 @@ def get_fruityvice_date(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-#dont run anything past here while we troubleshoot
-streamlit.stop()
+
 #New Section to display fruity api response
 streamlit.header('Fruityvice Fruit Advice!')
+#dont run anything past here while we troubleshoot
+streamlit.stop()
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
